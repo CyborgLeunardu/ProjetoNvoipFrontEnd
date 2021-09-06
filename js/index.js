dd968460ac90ccb0d3a144d4aa24b825bfd4b8f8
Aryane Ladeira <aryanemottals@gmail.com>
2021-09-06 14:51:48 -0300

    Update
    Criando popup e o script para abrir e fechar

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+function openPopup(idPopup){
+    document.getElementById(idPopup).style.display = "block"
+}
+
+function closePopup(idPopup){
+    document.getElementById(idPopup).style.display = "none"
+}
