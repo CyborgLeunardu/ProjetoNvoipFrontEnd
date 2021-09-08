2aabc617694285ab1f9a7aad6533e1349c703f69
Aryane Ladeira <aryanemottals@gmail.com>
2021-09-08 09:23:54 -0300

    Update
    Terminando o css dos popups

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 
 function openPopup(idPopup){
-    document.getElementById(idPopup).style.display = "block"
+    var popup = document.getElementById(idPopup)
+    popup.style.display = "block"
+
+
+    
 }
 
 function closePopup(idPopup){
-    document.getElementById(idPopup).style.display = "none"
+    document.getElementById(idPopup).style.display = "none"  
 }
